5d9a9b0995293991ecf648e6e1d47493933790c4
NUCBOX_M7PRO\Rentaro Kinoshita <rk7fd3s@gmail.com>
2025-01-08 17:25:23 +0900

    登録API作成

--- src/app/api/nippo/route.ts
+++ src/app/api/nippo/route.ts
@@ -1,7 +1,15 @@
+import { Article } from "@/app/types";
 import { supabase } from "@/utils/supabaseClient";
 import { NextResponse } from "next/server";
 
+/**
+ * 日報データ全件取得
+ * @param req リクエスト
+ * @param res レスポンス
+ * @returns レスポンス
+ */
 export async function GET(req: Request, res: Response) {
+  // supabaseから登録降順に全データ取得
   const { data, error } = await supabase
     .from("posts")
     .select("*")
@@ -16,3 +24,28 @@ export async function GET(req: Request, res: Response) {
 
   return NextResponse.json(data, { status: 200 });
 }
+
+/**
+ * 新規登録
+ * @param req リクエスト
+ * @param res レスポンス
+ * @returns レスポンス
+ */
+export async function POST(req: Request, res: Response) {
+  const item: Article = await req.json();
+
+  // id, created_atを抜いたオブジェクトを生成
+  const {id, created_at, ...reqBody} = item;
+
+  // supabaseにデータ登録
+  const { data, error } = await supabase
+    .from("posts")
+    .insert([reqBody]);
+
+  if (error) {
+    return NextResponse.json(error);
+  }
+
+  return NextResponse.json(data, { status: 201 });
+}
+
